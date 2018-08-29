8080a1eec3c7ce3dea78b4066cc3b93424505e76
qianshuang <2009qianshuang@163.com>
2018-08-29 14:18:56 +0800

    run

--- run.py
+++ run.py
@@ -25,13 +25,13 @@ def get_time_dif(start_time):
     return timedelta(seconds=int(round(time_dif)))
 
 
-def feed_data(x_batch, y_batch, keep_prob):
-    feed_dict = {
-        model.input_x: x_batch,
-        model.input_y: y_batch,
-        model.keep_prob: keep_prob,
-    }
-    return feed_dict
+# def feed_data(x_batch, y_batch, keep_prob):
+#     feed_dict = {
+#         model.input_x: x_batch,
+#         model.input_y: y_batch,
+#         model.keep_prob: keep_prob,
+#     }
+#     return feed_dict
 
 
 def evaluate(sess, x_, y_):
@@ -42,7 +42,12 @@ def evaluate(sess, x_, y_):
     total_acc = 0.0
     for x_batch, y_batch in batch_eval:
         batch_len = len(x_batch)
-        feed_dict = feed_data(x_batch, y_batch, 1.0)
+        # feed_dict = feed_data(x_batch, y_batch, 1.0)
+        feed_dict = {
+            model.input_x: x_batch,
+            model.input_y: y_batch,
+            model.keep_prob: 1.0,
+        }
         loss, acc = sess.run([model.loss, model.acc], feed_dict=feed_dict)
         total_loss += loss * batch_len
         total_acc += acc * batch_len
@@ -77,7 +82,12 @@ def train():
         print('Epoch:', epoch + 1)
         batch_train = batch_iter(x_train, y_train, 128)
         for x_batch, y_batch in batch_train:
-            feed_dict = feed_data(x_batch, y_batch, config.dropout_keep_prob)
+            # feed_dict = feed_data(x_batch, y_batch, config.dropout_keep_prob)
+            feed_dict = {
+                model.input_x: x_batch,
+                model.input_y: y_batch,
+                model.keep_prob: config.dropout_keep_prob
+            }
 
             if total_batch % config.print_per_batch == 0:
                 # 每多少轮次输出在训练集和验证集上的性能
